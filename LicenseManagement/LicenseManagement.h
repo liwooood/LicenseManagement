cc49498675b7f5ab921a3170751f186d9dddafa2
anderschen <chenhf2010@qq.com>
2014-01-21 17:47:20 +0800

    授权管理

---
+++ LicenseManagement/LicenseManagement.h
@@ -0,0 +1,32 @@
+
+// LicenseManagement.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CLicenseManagementApp:
+// 有关此类的实现，请参阅 LicenseManagement.cpp
+//
+
+class CLicenseManagementApp : public CWinApp
+{
+public:
+	CLicenseManagementApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CLicenseManagementApp theApp;
